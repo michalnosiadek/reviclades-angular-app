a57b52915324cdac24b85e3cbb45e887c9fc6eec
michalnosiadek <nosiadek.michal@gmail.com>
2023-06-29 18:43:51 +0200

    removed-light-theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,24 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    // Check for saved theme preference.
-    const currentTheme = localStorage.getItem('theme');
-    if (currentTheme) {
-      document.documentElement.setAttribute('data-theme', currentTheme);
-    }
-  }
-
+export class AppComponent {
   title = 'reviclades-angular-app';
-
-  toggleTheme() {
-    if (document.documentElement.getAttribute('data-theme') === 'dark') {
-      document.documentElement.setAttribute('data-theme', 'light');
-      localStorage.setItem('theme', 'light'); // Save user preference
-    } else {
-      document.documentElement.setAttribute('data-theme', 'dark');
-      localStorage.setItem('theme', 'dark'); // Save user preference
-    }
-  }
 }
