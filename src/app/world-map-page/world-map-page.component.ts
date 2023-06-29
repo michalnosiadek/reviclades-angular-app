8dcf586dba01f585395ed42c61fb2689e2473ac7
michalnosiadek <nosiadek.michal@gmail.com>
2023-06-29 17:19:42 +0200

    added-all-necessary-components

---
+++ src/app/world-map-page/world-map-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-world-map-page',
+  templateUrl: './world-map-page.component.html',
+  styleUrls: ['./world-map-page.component.css']
+})
+export class WorldMapPageComponent {
+
+}
